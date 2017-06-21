284181cdaaa99999bd127cdac2cd33cd96b78662
Chiahao Lin <y80811@gmail.com>
2017-06-21 23:14:33 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+
+import Main from './components/Main/'
+import store from './store/'
+import history from './store/history'
+
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Main />
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+)
+registerServiceWorker()
---
+++ src/store/configurationStore.js
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import reduxThunk from 'redux-thunk'
+import reduxPromiseMiddleware from 'redux-promise-middleware'
+import { createLogger } from 'redux-logger'
+import { routerMiddleware } from 'react-router-redux'
+
+import rootReducer from '../reducers/'
+import initialState from './initialState'
+import history from './history'
+
+export default createStore(
+    rootReducer,
+    initialState,
+    compose(
+        applyMiddleware(
+            createLogger(),
+            reduxThunk,
+            reduxPromiseMiddleware(),
+            routerMiddleware(history)
+        ),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    )
+)
